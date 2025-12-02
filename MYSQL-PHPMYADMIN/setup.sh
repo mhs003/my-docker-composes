0f2218be1507a28fe07e2f072797a0277929ac08
Monzurul Hasan <monzurulhasan1001@gmail.com>
2025-12-02 11:51:20 +0600

    feat: add setup scripts

---
+++ MYSQL-PHPMYADMIN/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p mysql_data
+sudo chown root:root mysql_data
+if [ $? -eq 0 ]; then
+    docker compose up -d
+else
+    rm -rf mysql_data
+fi
+
+echo -e "\n\e[1;32m Complete!! \e[0m\n"
